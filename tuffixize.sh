5642f05e3581f6af09e94dffdf4296da532957b4
Kevin Wortman <kwortman@gmail.com>
2018-05-25 12:08:27 -0700

    save playbook locally

--- tuffixize.sh
+++ tuffixize.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-URL="https://raw.githubusercontent.com/kevinwortman/tuffix/master/tuffix.yml"
-
-ANSIBLE="ansible-playbook --inventory localhost, --connection local"
-
 if (( EUID == 0 )); then
     echo "error: do not run tuffixize.sh as root"
     exit 1
@@ -11,6 +7,10 @@ fi
 
 sudo apt --yes install ansible wget
 
-wget $URL | sudo $ANSIBLE
+wget https://raw.githubusercontent.com/kevinwortman/tuffix/master/tuffix.yml
+
+sudo ansible-playbook --inventory localhost, --connection local
+
+rm -f tuffix.yml
 
 sudo chown -R $USER:$USER ~/.atom
