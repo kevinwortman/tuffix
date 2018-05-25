9bd7c62e3ffd40e65633485e3b1f172ff684d492
Kevin Wortman <kwortman@gmail.com>
2018-05-25 12:00:46 -0700

    variable quoting

--- tuffixize.sh
+++ tuffixize.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-TUFFIX_YML_URL=https://raw.githubusercontent.com/kevinwortman/tuffix/master/tuffix.yml
+TUFFIX_YML_URL="https://raw.githubusercontent.com/kevinwortman/tuffix/master/tuffix.yml"
 
-ANSIBLE_COMMAND=ansible-playbook --inventory localhost, --connection local
+ANSIBLE_COMMAND="ansible-playbook --inventory localhost, --connection local"
 
 if (( EUID == 0 )); then
     echo "error: do not run tuffixize.sh as root"
     exit 1
 fi
 
-apt --yes install ansible wget
+sudo apt --yes install ansible wget
 
 wget $(TUFFIX_YML_URL) | sudo $(ANSIBLE_COMMAND)
 
