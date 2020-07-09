a822560a1522136f78b8fffa2d42400714ee24c3
JaredDyreson <jared.dyreson@gmail.com>
2020-07-08 23:42:09 -0600

    testing to see if vscode implementation works as planned

--- tool/tuffixlib.py
+++ tool/tuffixlib.py
@@ -385,23 +385,26 @@ class BaseKeyword(AbstractKeyword):
         print(f'[INFO] Adding all packages to the APT queue ({len(self.packages)})')
         # print("[INFO] Adding all packages to APT queue...")
         add_deb_packages(self.packages)
-        self.atom()
-        self.google_test_all()
-        self.configure_git()
+        # self.atom()
+        # self.google_test_all()
+        # self.configure_git()
       
     def remove(self):
         remove_deb_packages(self.packages)
 
     def add_vscode_repository(self):
-        script = """
-        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
-        sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-        """
+        sudo_install_command = "sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/trusted.gpg.d/"
 
-        for command in script:
-            subprocess.run(command.split())
+        curl_request = subprocess.Popen(("curl", "https://packages.microsoft.com/keys/microsoft.asc"),
+                                        stdout=subprocess.PIPE)
+        gpg_command = subprocess.check_output(('gpg', '--dearmor'), stdin=curl_request.stdout)
+        microsoft_gpg = pathlib.Path("/tmp/packages.microsoft.gpg")
+        with open(microsoft_gpg, "wb") as fp:
+            fp.write(gpg_command)
+        subprocess.run(sudo_install_command.split())
+
         vscode_source = pathlib.Path("/etc/apt/sources.list.d/vscode.list")
-        vscode_ppa = "\"deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main\""
+        vscode_ppa = "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main"
         with open(vscode_source, "w") as fp:
             fp.write(vscode_ppa)
 
