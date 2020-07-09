07c4efd9a22a4c67f1cf8224edbaac7ab2d34110
JaredDyreson <jared.dyreson@gmail.com>
2020-07-08 22:21:48 -0700

    do not want to work in vm anympre

--- tool/example.py
+++ tool/example.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python3
 import subprocess
+import pathlib
+import requests
 
 script = """
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
 sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 """
 
-for command in script:
-    subprocess.run(command.split())
+curl_request = subprocess.Popen(("curl", ""))
+quit()
+asc_file = pathlib.Path("/tmp/microsoft.asc")
+with open(asc_file, "wb") as fp:
+    fp.write(requests.get("https://packages.microsoft.com/keys/microsoft.asc").content)
+quit()
+
+for command in script.splitlines():
+    if(command):
+        subprocess.run(command.strip().split())
 
 vscode_source = pathlib.Path("/etc/apt/sources.list.d/vscode.list")
 vscode_ppa = "\"deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main\""
